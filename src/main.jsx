40b8f9b4ed80e9e2017497c9e80a92d153658a73
JesusLopezGomez <jelopez.gom@gmail.com>
2024-02-04 11:05:24 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import Impresoras3d from './impresoras3d.jsx'
 import Impresoras3dById from './impresoras3dById.jsx'
 import AddImpresora3d from './addImpresora3d.jsx'
+import { Navbar } from './navBar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar></Navbar>
+    <br />
     <Impresoras3d></Impresoras3d>
-    <br></br>
+    <br />
     <Impresoras3dById id={"6564516a831d901024fa6f01"}></Impresoras3dById>
-    <br></br>
+    <br />
     <AddImpresora3d></AddImpresora3d>
   </React.StrictMode>,
 )
