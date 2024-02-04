338cdabbd12c8b1fbbbdde97344d274b76ed46c3
JesusLopezGomez <jelopez.gom@gmail.com>
2024-02-04 12:04:52 +0100

    .

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,7 +16,7 @@ const Layout = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -25,23 +25,44 @@ const Layout = () => {
               <li className="nav-item">
                 <Link to="/" className="nav-link active">Home</Link>
               </li>
-              <li className="nav-item">
-                <Link to="/impresoras3d" className="nav-link active">Impresoras 3D</Link>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" id="impresorasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Impresoras 3D
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="impresorasDropdown">
+                  <li className="nav-item">
+                    <Link to="/impresoras3d" className="dropdown-item">Ver todas</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/addImpresora3d" className="dropdown-item">Añadir impresora 3D</Link>
+                  </li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <Link to="/addImpresora3d" className="nav-link active">Añadir impresora 3D </Link>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" id="repuestosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Repuestos
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="repuestosDropdown">
+                  <li className="nav-item">
+                    <Link to="/repuestos" className="dropdown-item">Ver todos</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/manageRepuesto" className="dropdown-item">Añadir repuesto</Link>
+                  </li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <Link to="/repuestos" className="nav-link active">Repuestos</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/manageRepuesto" className="nav-link active">Añadir repuesto</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/filamentos" className="nav-link active">Filamentos</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/manageFilamento" className="nav-link active">Añadir filamento</Link>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" id="filamentosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Filamentos
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="filamentosDropdown">
+                  <li className="nav-item">
+                    <Link to="/filamentos" className="dropdown-item">Ver todos</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/manageFilamento" className="dropdown-item">Añadir filamento</Link>
+                  </li>
+                </ul>
               </li>
               <li className="nav-item">
                 <Link to="/register" className="nav-link active">Registro</Link>
@@ -54,6 +75,7 @@ const Layout = () => {
           </div>
         </div>
       </nav>
+
       <Outlet />
     </>
   );
